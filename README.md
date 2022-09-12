9e65cfeb49cc9475c52127d354ac741e032e5b90
saf-s <78832241+saf-s@users.noreply.github.com>
2022-09-12 01:05:48 -0500

    Update files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# nlp_portfolio
-Portfolio to showcase work for natural language processing (NLP)
+# NLP Portfolio
+Portfolio to showcase work from CS 4395 (Natural Language Processing)
+
+## Overview of NLP
+In this [overview of NLP](overview_of_NLP.pdf), I define NLP, describe the relationship between AI and NLP, compare and contrast natural language understanding and natural language generation, list some examples of modern NLP applications, describe the 3 main approaches to NLP with examples, and describe my personal interest in NLP.
+
+## Assignment 1: Text Processing
+[This program](text_processing.py) reads in an [employee file](data.csv), processes the text to be more standardized, creates an object for each person with corrections from the user, and displays the information of each person.
+
+This program can be run on the Python terminal by entering the name of the program with the relative path *data/data.csv* as a system argument, where [data.csv](data.csv) is the file that should be located inside a folder named *data*. Example: text_processing.py data/data.csv
+
+Python is useful for text processing because it has modules like the Natural Language Toolkit (NLTK) library with functions to process text easily, such as parsing using tokens. Python's weakness when it comes to text processing is that an interpreted language like Python can run slow for programs that require processing large amounts of data.
+
+Through this assignment, I learned how to work with regular expressions to process text to match a specific format and with pickling/unpickling to save a dictionary of employee data to file for reading. I also reviewed sysarg, file I/O, and creating classes in Python.
+
+## Assignment 2: Exploring NLTK
+[This Python notebook](exploring_NLTK.pdf) explores the NLTK library in Python for performing NLP functions such as tokenizing and text preprocessing like stemming and lemmatization.
---
+++ text_processing.py
@@ -0,0 +1,99 @@
+# Assignment 1: Text Processing with Python
+# This program reads in an input file (data/data.csv) that contains employee information, processes the text to be more standardized, creates an object for each person with corrections from the user, and displays the updated information of each person.
+
+import sys, os, csv, re, pickle
+
+# if sysarg does not include relative path of input file, prints error message and ends program
+if len(sys.argv) < 2:
+        print("Error: Please enter the relative path of the input file as a system arg")
+        exit(1)
+
+# Person class for employee data from input file fields (last, first, mi, id, phone)
+class Person:
+    # constructor method that initializes object attributes
+    def __init__(self, last, first, mi, id, phone):
+        self.last = last
+        self.first = first
+        self.mi = mi
+        self.id = id
+        self.phone = phone
+
+    # method that outputs fields for each employee from input file
+    def display(self):
+        print("Employee ID: ", self.id)
+        print("\t     ", self.first, self.mi, self.last)
+        print("\t     ", self.phone, "\n")
+
+# main function
+def main():
+    # sets dictionary for employees from input file
+    persons = {}
+    persons = process_text(persons)
+
+    # saves dictionary as pickle file using write binary
+    pickle.dump(persons, open('persons.p', 'wb'))
+    # opens and reads pickle file using read binary
+    persons_in = pickle.load(open('persons.p', 'rb'))
+
+    # loops through dict of persons to display each person by verifying unpickling
+    print("\nEmployee list:\n")
+    for person in persons_in.keys():
+        persons_in[person].display()
+
+# function that processes text from input file
+def process_text(persons):
+    # user specifies the relative path ‘data/data.csv’ in a sysarg
+    filepath = sys.argv[1]
+    # opens input file for reading
+    with open(os.path.join(os.getcwd(), filepath), 'r') as f:
+        # reads in input file text starting from second line to get rid of heading
+        f.readline()
+        # splits text on comma to get fields as text variables
+        csv_reader = csv.reader(f)
+        # loops through fields for each employee in each row
+        for row in csv_reader:
+            text = row
+
+            # declares variables for each field
+            last = text[0]
+            first = text[1]
+            mi = text[2]
+            id = text[3]
+            phone = text[4]
+
+            # modifies first letter of last name and first name to be capitalized
+            last = last.capitalize()
+            first = first.capitalize()
+
+            # modifies middle initial to be a single upper case letter and adds 'X' for missing middle initials
+            if not mi:
+                mi = "X"
+            mi = mi.capitalize()
+
+            # uses regex to validate ID format by modifying incorrect ID forms
+            while re.match('[A-Za-z][A-Za-z]\d{4}', id) is None:
+                print("ID invalid:", id)
+                print("ID is two letters followed by 4 digits")
+                id = input("Please enter a valid ID: ")
+                # checks for duplicate ID and prints error message if ID already exists in input file
+                #if id in persons.keys():
+                    #print("Person with ID ", id, " already exists in file")
+
+            # modifies phone number to be in form 999-999-9999
+            while re.match('\w{3}-\w{3}-\w{4}', phone) is None:
+                print("Phone", phone, "is invalid")
+                print("Enter phone number in form 123-456-7890")
+                phone = input("Enter phone number: ")
+
+            # creates Person object
+            person = Person(last, first, mi, id, phone)
+            
+            # saves object to a dict of persons, where id is the key
+            persons[id] = person
+    
+    # returns dict of persons to main function
+    return persons
+
+# calls main function
+if __name__ == '__main__':
+    main()
